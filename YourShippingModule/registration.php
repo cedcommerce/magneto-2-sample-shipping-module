1841e1433d3985b5b1c4d084b82306932657feb3
CedCommerce <cedcommerce@gmail.com>
2015-11-17 17:45:52 +0530

    updated module according to Magento 2.0.0-rc2

---
+++ YourShippingModule/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'YourCompanyName_YourShippingModule',
+    __DIR__
+);
